15b3fd5019a8d7e13fb99e6d894d60819e6f484d
FranciscoMColino <francisco.m.colino@gmail.com>
2024-06-14 17:54:27 +0100

    Initial integration with oc-sort

--- adonis_mot/adonis_mot/ocsort_cluster_bbox_viz.py
+++ adonis_mot/adonis_mot/ocsort_cluster_bbox_viz.py
@@ -5,6 +5,7 @@ import sensor_msgs_py.point_cloud2 as pc2
 from ember_detection_interfaces.msg import EmberClusterArray
 import numpy as np
 import open3d as o3d
+from .ocsort_tracker.ocsort import OCSort
 
 def load_pointcloud_from_ros2_msg(msg):
     pc2_points = pc2.read_points_numpy(msg, field_names=("x", "y", "z"), skip_nans=True)
@@ -19,6 +20,23 @@ def get_corners_from__8point_bbox(points):
     corners[1] = np.max(points, axis=0)
     return corners
 
+def get_2d_bbox_from_3d_bbox(points):
+    # returns the min and max corners of the bounding box
+    corners = np.zeros((2, 2))
+    corners[0] = np.min(points[:, :2], axis=0)
+    corners[1] = np.max(points[:, :2], axis=0)
+    return corners
+
+def get_track_struct_from_2d_bbox(bbox):
+    # returns the min and max corners of the bounding box
+    track = np.zeros(5)
+    track[0] = bbox[0][0]
+    track[1] = bbox[0][1]
+    track[2] = bbox[1][0]
+    track[3] = bbox[1][1]
+    track[4] = 0.9 # dummy confidence score
+    return track
+
 class ClusterBoundingBoxViz(Node):
     def __init__(self):
         super().__init__('cluster_bbox_viz')
@@ -27,6 +45,12 @@ class ClusterBoundingBoxViz(Node):
         self.vis = o3d.visualization.Visualizer()
         self.vis.create_window('Open3D', width=640, height=480)
         self.setup_visualizer()
+
+        self.id_to_color = {
+            0: (0.3, 0.3, 0.3),
+        }
+
+        self.ocsort = OCSort(det_thresh=0.5)
 
     def setup_visualizer(self):
         # Add 8 points to initiate the visualizer's bounding box
@@ -62,28 +86,53 @@ class ClusterBoundingBoxViz(Node):
 
         ember_cluster_array = msg.clusters
 
-        for ember_cluster in ember_cluster_array:
+        bboxes_array = np.array([get_2d_bbox_from_3d_bbox(np.array([[p.x, p.y, p.z] for p in ember_cluster.bounding_box.points])) for ember_cluster in ember_cluster_array])
+        bboxes_to_track = np.array([get_track_struct_from_2d_bbox(bbox) for bbox in bboxes_array])
+
+        mock_img_info = np.array([640, 480])
+        mock_img_size = np.array([640, 480])
+
+        tracking_res = self.ocsort.update(bboxes_to_track, mock_img_info, mock_img_size)
+
+
+
+        for i in range(len(ember_cluster_array)):
+            ember_cluster = ember_cluster_array[i]
             ember_bbox = ember_cluster.bounding_box
             ember_pc2 = ember_cluster.point_cloud
+
+            # track id is last element in the tracking res, find using the bbox
+            # find track result where 4 first elements match the bbox
+            track_id = 0
+
+            for track in tracking_res:
+                if np.allclose(track[:4], bboxes_to_track[i][:4]):
+                    track_id = track[4]
+                    break
+
+            if track_id not in self.id_to_color:
+                self.id_to_color[track_id] = np.random.rand(3)
+            
+            color = self.id_to_color[track_id]
+
+            bbox_points = np.array([[p.x, p.y, p.z] for p in ember_bbox.points])
+
+            box_pc = o3d.geometry.PointCloud()
+            box_pc.points = o3d.utility.Vector3dVector(bbox_points)
+            self.vis.add_geometry(box_pc, reset_bounding_box=False)
+
+            bbox = box_pc.get_axis_aligned_bounding_box()
+            bbox.color = color
+            self.vis.add_geometry(bbox, reset_bounding_box=False)
 
             cluster_points = load_pointcloud_from_ros2_msg(ember_pc2)
 
             cluster_point_cloud = o3d.geometry.PointCloud()
             cluster_point_cloud.points = o3d.utility.Vector3dVector(cluster_points)
-            cluster_point_cloud.paint_uniform_color((0, 0, 1))
+            cluster_point_cloud.paint_uniform_color(color)
             self.vis.add_geometry(cluster_point_cloud, reset_bounding_box=False)
-
-            if ember_bbox.points_count.data != 8:
-                print('Invalid bounding box with {} points'.format(ember_bbox.points_count))
-                continue
-            points_np = np.array([[p.x, p.y, p.z] for p in ember_bbox.points])
-            corners = get_corners_from__8point_bbox(points_np)
-
-            # display only corners of the bounding box
-            corners_pc = o3d.geometry.PointCloud()
-            corners_pc.points = o3d.utility.Vector3dVector(corners)
-            corners_pc.paint_uniform_color((1, 0, 0))
-            self.vis.add_geometry(corners_pc, reset_bounding_box=False)
+            
+            
 
         self.vis.poll_events()
         self.vis.update_renderer()
